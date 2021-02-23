d4c42868154e93fcd3e1c5451566cf693aaaefcf
Pedro <pedroianmoraes@gmail.com>
2021-02-23 11:48:48 -0300

    Update letraB_1.m

--- letraB_1.m
+++ letraB_1.m
@@ -1,5 +1,22 @@
 syms t;
 
+%%%%
+
+L(1) = Revolute('d', .290, 'alpha', -pi/2, 'qlim', (11/12)*[-pi pi]);
+L(2) = Revolute('a', .270, 'offset', -pi/2, 'qlim', (11/18)*[-pi pi]);
+L(3) = Revolute('a', .070, 'alpha', -pi/2, 'qlim', [-(11/18)*pi (7/18)*pi]);
+L(4) = Revolute('d', 0.302, 'alpha', pi/2, 'qlim', (8/9)*[-pi pi]);
+L(5) = Revolute('alpha', -pi/2, 'qlim', (2/3*[-pi pi]));
+L(6) = Revolute('d', .072, 'offset', pi, 'qlim', (20/9)*[-pi pi]);
+
+i120 = SerialLink(L, 'name', 'IRB 120')
+
+q = [0 0 0 0 -pi/2 0];
+
+qdot_lim = pi*[25/18 25/18 25/18 16/9 16/9 7/3];
+
+%%%%
+
 wn = pi / 10;
 
 pds(t) = [0.05*sin(wn*t)+.428 .500 .05*cos(wn*t)+.569];
@@ -11,7 +28,7 @@ lambda = 1.8;
 
 e = inf(6 ,1);
 
-cont0= tic;
+cont0 = tic;
 
 t0 = tic;
 
@@ -25,3 +42,9 @@ Td = SE3(Rd, double(pds(contf)));
 
 rpyd = rotm2eul(Rd);
 
+
+%%%%
+
+T = i120.fkine(q);
+
+J = i120.jacob0(q, 'rpy');
