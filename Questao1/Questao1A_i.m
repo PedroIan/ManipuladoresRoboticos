e5688395c24983d8c9610164057054bd73c56e23
Pedro <pedroianmoraes@gmail.com>
2021-02-24 17:20:41 -0300

    1a_i completa e funcionando

--- Questao1/Questao1A_i.m
+++ Questao1/Questao1A_i.m
@@ -6,25 +6,101 @@ close all
 
 syms t;
 
-
 %% Inicialização dos Parâmetros
 
-L(1) = Revolute('d', .290, 'alpha', -pi/2, 'qlim', (11/12)*[-pi pi]);
-L(2) = Revolute('a', .270, 'offset', -pi/2, 'qlim', (11/18)*[-pi pi]);
-L(3) = Revolute('a', .070, 'alpha', -pi/2, 'qlim', [-(11/18)*pi (7/18)*pi]);
-L(4) = Revolute('d', 0.302, 'alpha', pi/2, 'qlim', (8/9)*[-pi pi]);
-L(5) = Revolute('alpha', -pi/2, 'qlim', (2/3)*[-pi pi]);
-L(6) = Revolute('d', .072, 'offset', pi, 'qlim', (20/9)*[-pi pi]);
+L(1) = Revolute('d', .290, 'alpha', -pi / 2, 'qlim', (11/12) * [-pi pi]);
+L(2) = Revolute('a', .270, 'offset', -pi / 2, 'qlim', (11/18) * [-pi pi]);
+L(3) = Revolute('a', .070, 'alpha', -pi / 2, 'qlim', [-(11/18) * pi (7/18) * pi]);
+L(4) = Revolute('d', 0.302, 'alpha', pi / 2, 'qlim', (8/9) * [-pi pi]);
+L(5) = Revolute('alpha', -pi / 2, 'qlim', (2/3) * [-pi pi]);
+L(6) = Revolute('d', .072, 'offset', pi, 'qlim', (20/9) * [-pi pi]);
 
 i120 = SerialLink(L, 'name', 'IRB 120')
 
-q = [0 0 0];
+q = [0 0 0 0 -pi / 2 0];
 
-qdot_lim = pi*[25/18 25/18 25/18 16/9 16/9 7/3];
+qdot_lim = pi * [25/18 25/18 25/18 16/9 16/9 7/3];
 
-%% Controle
+%% Definições de Controle
 
-desiredPosition = [0.38 .38 .5];
+posicaoInicial = [0 0 0 0 -pi / 2 0];
+
+posicaoDesejada = [0.38 .38 .5 0 0 0];
+
+T = i120.fkine(posicaoDesejada); % Pega pose desejada do efetuador 
+pd = transl(T); % Pega vetor de transla��o do efetuador
+Rd = SO3(T); % Pega o objeto SO3 correspondente � rota��o do efetuador
+Rd = Rd.R(); %Pega matriz de rota��o do efetuador
+
+ganho = 0.8;
+epsilon = 2e-2;
+
+e_ant = 1;
+e = 0; 
+
+%% Aplicação do Controle
+
+
+figure(1)
+i120.plot(q); % Plot rob� na configura��o inicial
+hold on
+T.plot('rgb') % Plot pose desejada
+%%
+i = 0
+
+testeTic = tic;
+
+while (norm(e - e_ant) > epsilon) % Crit�rio de parada
+    JCompleta = i120.jacob0(q, 'rpy'); % Jacobiana geom�trica
+    J = JCompleta(1:3,:)
+    T = i120.fkine(q); % Cinem�tica direta para pegar a pose do efetuador 
+    p = transl(T); % Transla��o do efetuador
+    R = SO3(T); 
+    R = R.R(); % Extrai rota��o do efetuador
+    i = i+1; % contador
+    
+    p_err = pd-p; % Erro de transla��o
+    
+    nphi = rotm2axang(Rd*R'); 
+    nphi_err = nphi(1:3)*nphi(4); % Erro de rota��o (n*phi)
+    
+    e_ant = e;
+    e = [p_err'; nphi_err']; % Vetor de erro
+    
+    u = pinv(J)*ganho*e(1:3,:); % Lei de controle
+
+    dt = toc(testeTic);
+    testeTic = tic;
+
+    q = q + dt*u'; % C�lculo de posicaoInicial (Regra do trap�zio)
+    
+    i120.plot(q);
+    control_sig(:,1) = u; % Sinal de controle
+    err(i) = norm(e); % Norma do erro
+    norm(e)
+end
+hold off
+
+%% Plot sinal de controle e norma do erro
+
+figure(2)
+hold on
+for i = 1:6
+    plot(control_sig(i,:))
+end
+hold off
+xlabel('Itera��es')
+ylabel('Sinal de controle: u(rad/s)')
+
+figure(3)
+plot(err)
+xlabel('Itera��es')
+ylabel('Norma do erro: |e|')
+box off
+
+%% Posição final Desejada
+
+desiredPosition = [0.38 .38 .5 0 0 0];
 
 T = i120.fkine(desiredPosition);
 
