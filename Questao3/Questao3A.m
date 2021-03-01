ad938f03f83e77ae3a81d6bd02cf3fa8b7f7cbec
Pedro <pedroianmoraes@gmail.com>
2021-03-01 14:55:10 -0300

    inicio conexão e testes coppelia

--- Questao3/Questao3A.m
+++ Questao3/Questao3A.m
@@ -18,16 +18,25 @@ clientID = sim.simxStart('127.0.0.1', 19999, true, true, 5000, 5);
 if (clientID >- 1)
     disp('Connected to remote API server');
 
-    h = [0 0 0 0 0 0 0]
-
-    [r, h(1)] = sim.simxGetObjectHandle(clientID, 'joint_7', sim.simx_opmode_blocking)
-    [r, h(2)] = sim.simxGetObjectHandle(clientID, 'joint_1', sim.simx_opmode_blocking)
-    [r, h(3)] = sim.simxGetObjectHandle(clientID, 'joint_2', sim.simx_opmode_blocking)
-    [r, h(4)] = sim.simxGetObjectHandle(clientID, 'joint_3', sim.simx_opmode_blocking)
-    [r, h(5)] = sim.simxGetObjectHandle(clientID, 'joint_4', sim.simx_opmode_blocking)
-    [r, h(6)] = sim.simxGetObjectHandle(clientID, 'joint_5', sim.simx_opmode_blocking)
-    [r, h(7)] = sim.simxGetObjectHandle(clientID, 'joint_6', sim.simx_opmode_blocking)
-
+    h = [0 0 0 0 0 0 0];
+    
+    joint_position = [pi/2 pi/2 0 0 0 pi/2 0];
+    
+    [r, h(1)] = sim.simxGetObjectHandle(clientID, 'joint_7', sim.simx_opmode_blocking);
+    [r, h(2)] = sim.simxGetObjectHandle(clientID, 'joint_1', sim.simx_opmode_blocking);
+    [r, h(3)] = sim.simxGetObjectHandle(clientID, 'joint_2', sim.simx_opmode_blocking);
+    [r, h(4)] = sim.simxGetObjectHandle(clientID, 'joint_3', sim.simx_opmode_blocking);
+    [r, h(5)] = sim.simxGetObjectHandle(clientID, 'joint_4', sim.simx_opmode_blocking);
+    [r, h(6)] = sim.simxGetObjectHandle(clientID, 'joint_5', sim.simx_opmode_blocking);
+    [r, h(7)] = sim.simxGetObjectHandle(clientID, 'joint_6', sim.simx_opmode_blocking);
+    
+    
+    for i=1:7
+        sim.simxSetJointTargetPosition(clientID, h(i), joint_position(i), sim.simx_opmode_streaming)
+    end
+    
+    [tete, position] = sim.simxGetJointPosition(clientID, h(2), sim.simx_opmode_streaming)
+    pause(10);
     %sim.simxGetJointMatrix(clientID)
 else
     disp('Failed connecting to remote API server');
